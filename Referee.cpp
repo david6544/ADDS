e5ffe72b158b688e6cbd9514bbeee05811be2ec9
david6544 <david.maslov@student.adelaide.edu.au>
2023-03-01 15:08:32 +1030

    finished prac1

--- Referee.cpp
+++ Referee.cpp
@@ -2,19 +2,32 @@
 #include"Referee.h"
 #include<string>
 #include<iostream>
+#include<unordered_map>
 
 Referee::Referee() {
 
 }
 
-Player* Referee::refGame(Player * human, Player * comp) {
-        
-    char move = human->makeMove();
+Player* Referee::refGame(Player * p1, Player * p2) {
+    
+    std::unordered_map<char,char> moves {
+        {'R','P'},
+        {'S','R'},
+        {'P','S'}
+    };
 
-    if (move == 'P') {
-        return comp;
-    } if (move == 'S') {
-        return human;
+    char m1 = p1->makeMove();
+    char m2 = p2->makeMove();
+
+    //std::cout << m1 << " " << m2 << std::endl;
+
+    if (m1 == m2) return nullptr;
+
+
+    if (m2 == moves[m1]) {
+        return p2;
+    } else {
+        return p1;
     }
 
     return nullptr;

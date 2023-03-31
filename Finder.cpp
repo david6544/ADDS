e26f9da4571d857daa1e03696e65ac6d54a01872
david6544 <david.maslov@student.adelaide.edu.au>
2023-03-31 23:32:32 +1030

    removed main

--- Finder.cpp
+++ Finder.cpp
@@ -37,7 +37,7 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
     return res;
 }
 
- 
+ /* 
 int main(void) {
     std::string s1 = "4634554567";
     std::string s2 = "45689";
@@ -52,4 +52,4 @@ int main(void) {
     }
 
     return 0;
-}
+} */

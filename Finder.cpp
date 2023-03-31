b4f64b63dc132a223e144d6ecd5681361a8e3331
david6544 <david.maslov@student.adelaide.edu.au>
2023-03-31 17:08:49 +1030

    test

--- Finder.cpp
+++ Finder.cpp
@@ -1,25 +1,23 @@
 #include "Finder.h"
+#include<iostream>
 
 using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
 
-    vector<int> result (s2.size(),-1);
+    vector<int> res (s2.size(), -1);
 
-    for(size_t i = 1; i <= s2.size(); i++) {
-        size_t found = -1;
-        for (int j = 0; j < s1.size(); j++) {
-            if (s1[j] != s2[0]) continue;
-            found = s1.find(s2.substr(0, i));
-            if (found == -1) break; 
-        }
-        if (found != string::npos) {
-            result[i-1] = found;
-        }
+    for (size_t i = 1; i <= s2.size(); i++) {
+
+            size_t found = s1.find(s2.substr(0, i)); //O(i*n)
+
+            if (found != string::npos) {
+                res[i-1] = found;
+            }
     }
-    return result;
+    return res;
 }
-/* 
+ 
 int main(void) {
     std::string s1 = "4634554567";
     std::string s2 = "45689";
@@ -35,6 +33,5 @@ int main(void) {
 
     return 0;
 }
- */
 
    

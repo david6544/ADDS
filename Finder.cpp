0ad8d9d0508479a867033bcee57e211d4a6339c8
david6544 <david.maslov@student.adelaide.edu.au>
2023-03-31 23:32:04 +1030

    testing

--- Finder.cpp
+++ Finder.cpp
@@ -5,24 +5,39 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> res (s2.size(), -1);
-    if (s1.size() == 0 || s2.size() == 0) return res;
+    //if (s1.size() == 0 || s2.size() == 0) return res;
 
-    for (size_t i = 1; i <= s2.size(); i++) {
+    /* for (size_t i = 1; i <= s2.size(); i++) {
             string curr = s2.substr(0,i);
 
-            for (int j = 0; j < s1.size() - curr.size(); j++) {
-                //cout << s1.substr(j,curr.size()) << endl;
+            int startpoint = res[i-1];
+            if (startpoint == -1) startpoint = 0;
+
+            for (int j = startpoint; j < s1.size() - curr.size(); j++) {
+                cout << s1.substr(j,curr.size()) << endl;
                 if (curr == s1.substr(j,curr.size())) {
                     res[i-1] = j;
                     break;
                 }
             }
 
+    } */
+
+    for (size_t i = 1; i <= s2.size(); i++) {
+        size_t found = s1.find(s2.substr(0,i));
+
+        if (found != string::npos) {
+            res[i-1] = found;
+        } else {
+            return res;
+        }
+
     }
+
     return res;
 }
 
- /* 
+ 
 int main(void) {
     std::string s1 = "4634554567";
     std::string s2 = "45689";
@@ -37,4 +52,4 @@ int main(void) {
     }
 
     return 0;
-} */
+}

d8eedbe33c8873564fb34a14be56017aeea9d472
david6544 <david.maslov@student.adelaide.edu.au>
2023-05-01 17:51:37 +0930

    fixed issues

---
+++ bigNumCalc.cpp
@@ -0,0 +1,28 @@
+#include "bigNumCalc.h"
+
+bigNumCalc::bigNumCalc()
+{
+}
+
+bigNumCalc::~bigNumCalc()
+{
+}
+
+std::list<int> bigNumCalc::buildBigNum(std::string numString) {
+    
+}
+
+std::list<int> bigNumCalc::add(std::list<int> num1, std::list<int> num2)
+{
+    return std::list<int>();
+}
+
+std::list<int> bigNumCalc::sub(std::list<int> num1, std::list<int> num2)
+{
+    return std::list<int>();
+}
+
+std::list<int> bigNumCalc::mul(std::list<int> num1, std::list<int> num2)
+{
+    return std::list<int>();
+}

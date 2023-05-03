ed89b2e6f82bc00110a672632c4d9d81d7deb7a5
David Maslov <a1870801@ingb15w027.ad.adelaide.edu.au>
2023-05-03 11:45:54 +0930

    did practical

---
+++ LinkedList.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include "LinkedList.h"
+
+LinkedList::LinkedList() {
+	head = nullptr;
+}
+
+LinkedList::~LinkedList() {
+	while (head != nullptr) {
+		deleteFront();
+	}
+}
+
+void LinkedList::insertFront(int item) {
+	Node* newNode = new Node(item, head);
+	head = newNode;
+}
+
+void LinkedList::deleteFront() {
+	if(head != nullptr) {
+		Node* oldHead = head;
+		head = head->link;
+		delete oldHead;
+	}
+}
+
+void LinkedList::printList() {
+	Node* currNode = head;
+	while(currNode != nullptr) {
+		std::cout << currNode->data << " ";
+		currNode = currNode->link;
+	}
+}
+
+/////////// your code goes here... DO NOT change the function signatures ///////////
+
+bool LinkedList::swap(int pos1, int pos2) {
+
+	if (pos1 == pos2) return true;
+	if (pos1 < 0 || pos2 < 0) return false;
+
+	if (pos1 < pos2) {
+		int temp = pos2;
+		pos2 = pos1;
+		pos1 = temp;
+	}
+	Node* Curr = head;
+
+	Node* Node1 = head;
+
+	int data1 = 0;
+
+	for (int i = 0; i < pos1 && Curr != nullptr; i++) {
+		if (i == pos2) {
+			data1 = Curr->data;
+			Node1 = Curr;
+		}
+		Curr = Curr->link;
+	}
+	
+	if (Curr != nullptr) {
+		Node1->data = Curr->data;
+		Curr->data = data1;
+	} else {
+		return false;
+	}
+
+	return true;
+}
+
+bool LinkedList::find_and_delete(int target) {
+	Node* Curr = head;
+	while (Curr->link != nullptr) {
+
+		//checks if the next node is the one to delete
+		if (Curr->link->data == target) {
+			//deletes the next node
+			Curr->link = Curr->link->link;
+			return true;
+		}
+		Curr = Curr->link;
+	}
+
+	return false;
+}

e2de10b3c4195b6250d0cb71da9603df6d64eb5b
david6544 <david.maslov@student.adelaide.edu.au>
2023-05-15 12:33:15 +0930

    finished kthlargest

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include"Heap.h"
 
 #include<iostream>
+#include<queue>
+#include<vector>
+
+int kth_largest(std::vector<int> values, int k) {
+    std::priority_queue<int> pQ;
+    for (auto x : values) {
+        pQ.push(x);
+    }
+
+    for (int i = 1; i < k; i++) {
+        pQ.pop();
+    }
+    return pQ.top();
+}
 
 int main(void) {
     Heap<int> h1 ({2,3,2,5,4,6,7,8,9,2});
@@ -21,6 +35,9 @@ int main(void) {
         std::cout << x << " ";
     }
     std::cout << std::endl;
+
+
+    std::cout << kth_largest({1,2,3,4,5,6,7,6,1,23,123,12,3}, 3) << std::endl;
 
     return 0;
 }  
